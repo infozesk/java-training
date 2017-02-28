1552553adb5a3daabdf74a14201432d986a4fb79
Zesk06 <zesk06@gmail.com>
2017-02-28 06:02:27 +0100

    add candy machine tests

--- exercices/exercice_15/src/test/java/org/infozesk/coffee/CandyMachineTest.java
+++ exercices/exercice_15/src/test/java/org/infozesk/coffee/CandyMachineTest.java
@@ -1,14 +1,31 @@
 package org.infozesk.coffee;
 
-import static org.junit.Assert.fail;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CandyMachineTest {
 
 	@Test
 	public void testGetCandy() {
-		fail("Not yet implemented");
+		CandyMachine machine = new CandyMachine();
+		Assert.assertFalse(machine.getCandy(CandyMachine.CROCODILE_TYPE));
+		Assert.assertFalse(machine.getCandy(CandyMachine.FRAISE_TYPE));
+		// un euro permet d'acheter 4 fraises
+		machine.addEuro();
+		Assert.assertTrue(machine.getCandy(CandyMachine.FRAISE_TYPE));
+		Assert.assertTrue(machine.getCandy(CandyMachine.FRAISE_TYPE));
+		Assert.assertTrue(machine.getCandy(CandyMachine.FRAISE_TYPE));
+		Assert.assertTrue(machine.getCandy(CandyMachine.FRAISE_TYPE));
+		// Pas plus de quatre
+		Assert.assertFalse(machine.getCandy(CandyMachine.FRAISE_TYPE));
+		// un euro permet d'acheter 3 crocos
+		machine.addEuro();
+		Assert.assertTrue(machine.getCandy(CandyMachine.CROCODILE_TYPE));
+		Assert.assertTrue(machine.getCandy(CandyMachine.CROCODILE_TYPE));
+		Assert.assertTrue(machine.getCandy(CandyMachine.CROCODILE_TYPE));
+		Assert.assertFalse(machine.getCandy(CandyMachine.CROCODILE_TYPE));
+		// et il reste 10cts
+		Assert.assertEquals(10, machine.getCents());
 	}
 
 	@Test
